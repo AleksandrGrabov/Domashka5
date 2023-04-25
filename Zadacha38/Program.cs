28a6cdb6c0ab8d15f40b43fdc5790f2c29bd680c
AleksandrGrabov <grabov-it@yandex.by>
2023-04-25 17:42:14 +0300

    Решили задачу 38

---
+++ Zadacha38/Program.cs
@@ -0,0 +1,23 @@
+﻿double[] array = { 3.21, 7.04, 22.93, -2.71, 78.24 };
+
+        double max = array[0];
+        double min = array[0];
+
+         for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > max)
+            {
+                max = array[i];
+            }
+
+            if (array[i] < min)
+            {
+                min = array[i];
+            }
+        }
+
+        double difference = max - min;
+
+        Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {difference}");
+
+        Console.ReadLine();
