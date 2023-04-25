1c0969bc64d97c77c76bb8ef725b00bd95396546
AleksandrGrabov <grabov-it@yandex.by>
2023-04-25 17:37:55 +0300

    Решили задачу 36

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] array = new int[4];
+        int sum = 0;
+
+        Random rand = new Random();
+
+          for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(-100, 100);
+            Console.WriteLine(array[i]);
+        }
+
+        for (int i = 1; i < array.Length; i += 2)
+        {
+            sum += array[i];
+        }
+
+        Console.WriteLine($"Сумма элементов с нечетными индексами: {sum}");
+
+        Console.ReadLine();
