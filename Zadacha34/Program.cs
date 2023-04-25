1bda889fddaeddc4697885967bb4ae4dd7916435
AleksandrGrabov <grabov-it@yandex.by>
2023-04-25 17:32:50 +0300

    Решили задачу 34

---
+++ Zadacha34/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = new int[4]; 
+        int count = 0; 
+
+        Random rand = new Random(); 
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rand.Next(100, 1000);
+            Console.WriteLine(array[i]);
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            {
+                count++; 
+            }
+        }
+
+              Console.WriteLine($"Количество четных чисел в массиве: {count}");
+
+        Console.ReadLine();
